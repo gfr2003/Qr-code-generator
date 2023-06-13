7d3a556cceb40d09448d5cfd82aa4f8a1550d0fb
Gustavo Felix <gu2003fr@outlook.com>
2023-06-12 23:12:25 -0300

    a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'qr-code-generator';
+  qrCode = '';
+  qrPath = '';
+  generateQr() {
+    this.qrCode = (document.getElementById('link') as HTMLInputElement).value;
+
+    if (!this.qrCode) {
+      alert('Insira um valor válido no campo!');
+      return;
+    }
+    this.qrPath = `https://chart.googleapis.com/chart?chs=150x150&cht=qr&chl=${this.qrCode}`;
+  }
 }
